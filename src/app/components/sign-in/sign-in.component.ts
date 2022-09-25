b5f0264bb897e9502695ddf3ff907d5f72b0f9ac
Shriram Mishra <ram.techcare27@gmail.com>
2022-09-25 17:17:27 +0530

    Nothing to add

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -8,9 +8,6 @@ import { AuthService } from '../../services/auth.service';
 })
 export class SignInComponent implements OnInit {
   title: string = "Login";
-
-
-
   email:string;
   password:string;
 
